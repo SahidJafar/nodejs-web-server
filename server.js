d0c8fd19d8ebef614ff528ff87dd454c3c6bd1c0
SahidJafar <sahid11.sj@gmail.com>
2022-03-11 00:51:02 +0700

    Membuat HTTP Server secara Native

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log('Halo, kita akan belajar membuat server');
+const http = require('http');
+ 
+/**
+ * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
+ * 
+ * @param request: objek yang berisikan informasi terkait permintaan
+ * @param response: objek yang digunakan untuk menanggapi permintaan
+ */
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+ 
+    response.statusCode = 200;
+    response.end('<h1>Halo HTTP Server!</h1>');
+};
+ 
+ 
+const server = http.createServer(requestListener);
+ 
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
+
+/**
+ * Cara Running npm run start
+ * Buka server berjalan
+ * atau buka cmd
+ * ketikkan curl -X GET http://localhost:5000/ --ssl-no-revoke
+ */
